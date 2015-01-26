51d84060958b4088807cd63ca477b209f9503965
avyishi <avyishi@gmail.com>
2015-01-25 21:19:54 -0800

    Fixed error with created movie under user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+
+  has_many :movies
 end
