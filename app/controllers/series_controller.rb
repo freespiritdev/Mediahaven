eb571984bd310d84a44679c3a737241bc73427f4
avyishi <avyishi@gmail.com>
2015-01-29 23:53:51 -0800

    Updated simple form in gemfile and series controller

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -7,6 +7,25 @@ class SeriesController < ApplicationController
     @serie = Serie.new
   end
 
+   def create
+    @serie = Serie.new(params[:serie])
+
+    if @serie.save
+      redirect_to series_path, notice: 'Show Added!'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @serie = Serie.find(params[:serie])
+    if @serie.update(serie_params)
+      redirect_to @serie, notice: 'TV Show successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
   def edit
   end
 
