f98b00d9de2eec0313041d95f904c35782a93408
Cheri <avyishi@gmail.com>
2015-03-02 22:28:26 -0800

    Updated welcome page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
   searchkick
   belongs_to :users
 
-  has_attached_file :image, styles: { medium: "200x200#" }
+  has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/tune.rb
+++ app/models/tune.rb
@@ -2,6 +2,6 @@ class Tune < ActiveRecord::Base
   searchkick
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "200x200#" }
+  has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
