acac311a13897dcecec1fcd5589e0f08ad2324c0
Cheri <avyishi@gmail.com>
2015-05-28 18:24:40 -0700

    Changed the order shows and tunes are displayed

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -4,4 +4,6 @@ class Tune < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  default_scope { order('created_at DESC') }
 end
