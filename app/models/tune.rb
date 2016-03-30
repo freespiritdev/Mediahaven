545639932beded8cc256e911fdc59bf59f2fe764
Cheri <avyishi@gmail.com>
2016-03-29 20:35:20 -0700

    Updated search in movies

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -3,7 +3,7 @@ class Tune < ActiveRecord::Base
   belongs_to :user
   has_many   :reviewtunes
 
-  has_attached_file :image, styles: { medium: "400x600#" }
+  has_attached_file :image, styles: { thumb: "100x100", small: "200x200", medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   default_scope { order('created_at DESC') }
