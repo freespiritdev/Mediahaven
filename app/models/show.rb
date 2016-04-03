5051f4ddedcc4882e9050f7b7f9593bbc55a5351
Cheri <avyishi@gmail.com>
2016-04-02 20:00:26 -0700

    Updated model

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   belongs_to :users
   has_many   :reviewtvs
 
-  has_attached_file :image, styles: { thumb: "100x100", small: "200x200", medium: "300x300", large: "600x600" }
+  has_attached_file :image, styles: { thumb: "100x100#", small: "200x200#", medium: "300x300#", large: "600x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   default_scope { order('created_at DESC') }
