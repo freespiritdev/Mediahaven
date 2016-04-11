deaeabc0969f8f1824e0369788940dd3235bcf4f
Cheri <avyishi@gmail.com>
2016-04-11 14:10:00 -0700

    Updated layout and models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
   
-  has_attached_file :image, styles: { thumb: "100x100#", small: "200x200#", medium: "300x300#", large: "600x600#" }
+  has_attached_file :image, styles: { thumb: "100x100#", small: "200x400#", medium: "400x600#", large: "600x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   default_scope { order('created_at DESC') }
